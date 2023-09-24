a6e99e21429b9b55cdcec8009788e1e83d814f1d
marcin-zajac <57039654+marcin-zajac@users.noreply.github.com>
2023-09-24 06:56:56 +0200

    feat: add routing and accountants path

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-import './i18n'
 import { useTranslation } from 'react-i18next'
+import { Route, Routes } from 'react-router-dom'
+import './i18n'
+import { Accountants } from './pages/accountants/Accountants'
 
 function App() {
   const { t } = useTranslation()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{t('translation_works')}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<h1>route: main</h1>} />
+        <Route path={`/${t('router.accountants')}`} element={<Accountants />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/accountants/Accountants.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Accountants = () => {
+  return <div>Accountants page component</div>
+}
